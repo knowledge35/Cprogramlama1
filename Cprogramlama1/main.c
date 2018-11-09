111cd4f1c22ec9f0fc17dc6952229d2f061953b4
scientiest35 <ibomyakmc@hotmasdail.com>
2018-11-09 06:59:24 +0300

    interesting

--- Cprogramlama1/main.c
+++ Cprogramlama1/main.c
@@ -5,14 +5,31 @@ int main() {
 	int a = 10;
 	int b = 3,c = 4;
 	int d;
+	{
+
+		// heyo
+		int a = 2;
+		printf("%d", a);
+
+	}
+	
 	printf("a:%d\n,b:%d\n,c:%d",a,b,c);
 	printf("bir sayi girinşiz");
 	scanf("%d",&d);
 	printf("\" a \b\t s \ngirilen sayi:%d",d);
 	float pi = 3.14;
+	
 	printf("\n%8.4f",pi);
 	/*interpretive*/
 	//compiler
+	
+	{
+
+		// interesting
+		int a =7;
+		printf("%d", a);
+
+	}
 	_getch();
 	
 	return 0;
